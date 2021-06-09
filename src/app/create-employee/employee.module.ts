1845a8ba751374293e06224eec36a9df0e04f037
Abdur Rehman <abdurrehman.syeds@gmail.com>
2021-06-09 16:49:27 +0530

    shared module

--- src/app/create-employee/employee.module.ts
+++ src/app/create-employee/employee.module.ts
@@ -2,7 +2,6 @@ import { EmployeeRoutingModule } from './employee-routing.module';
 import { ListEmployeeComponent } from './../list-employee/list-employee.component';
 import { CreateEmployeeComponent } from './create-employee.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 
 
